12ce53877f8e398c5d7685cc82c429ebc782b1a8
jackmock <jack.r.mock@gmail.com>
2025-05-20 15:11:32 -0500

    began implementing child class of HTMLNode- LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -7,7 +7,7 @@ class HTMLNode:
 
     def to_html(self):
         raise NotImplementedError
-        # child classes will override this method
+        # child classes override this method
 
     def props_to_html(self):
         result_string = ""
@@ -21,3 +21,20 @@ class HTMLNode:
     
     def __repr__(self):
         return(f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})")
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, value, tag=None, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError
+        props_str = ''
+        if self.props is not None:
+            for i in self.props:
+                props_str += (f' {i}="{self.props[i]}"')
+        if self.tag is None:
+            return f'{self.value}'
+        else:
+            return f'<{self.tag} {props_str}>{self.value}</{self.tag}>'
