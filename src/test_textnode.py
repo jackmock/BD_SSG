576e8aaf1c462dc1a754f5f5f2100f9a3c730e8d
jackmock <jack.r.mock@gmail.com>
2025-05-21 14:12:15 -0500

    added several unit tests to test the split_node_delimiter function in test_textnode.py file

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -2,6 +2,8 @@ import unittest
 
 from textnode import*
 
+import split_nodes_delimiter
+
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -24,6 +26,32 @@ class TestTextNode(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
+
+    def split_test_1_delimiter(self):
+        nodes = [TextNode("Look, `code` lives here!", TextType.TEXT)]
+        result = split_nodes_delimiter(nodes, "`", TextType.CODE)
+        self.assertEqual(result, [TextNode("Look, ", TextType.TEXT), TextNode("code", TextType.CODE), TextNode(" lives here!", TextType.TEXT)])
+
+    def split_test_2_delimiters(self):
+        nodes = [TextNode("First `block` then `second`.", TextType.TEXT)]
+        result = split_nodes_delimiter(nodes, "`", TextType.CODE)
+        self.assertEqual(result, [TextNode("First ", ...), TextNode("block", ...), ..., TextNode("second", ...), ...])
+
+    def split_test_no_delimiters(self):
+        nodes = [TextNode("Nothing special here.", TextType.TEXT)]
+        result = split_nodes_delimiter(nodes, "`", TextType.CODE)
+        self.assertEqual(result, [TextNode("Nothing special here.", TextType.TEXT)])
+
+    def split_test_unmatched_delimiter(self):
+        nodes = [TextNode("Oh no, this is `not closed!", TextType.TEXT)]
+        self.assertRaises(Exception)
+    
+    def split_test_non_text_type(self):
+        bold_node = TextNode("I am bold!", TextType.BOLD)
+        nodes = [bold_node]
+        result = split_nodes_delimiter(nodes, "`", TextType.CODE)
+        self.assertEqual(result, [bold_node])
+    
 
 if __name__ == "__main__":
     unittest.main()
