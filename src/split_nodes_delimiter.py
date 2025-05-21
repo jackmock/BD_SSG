ad0efb03dedbffd05ebf7a6b0a03adbe98f6c32f
jackmock <jack.r.mock@gmail.com>
2025-05-21 13:47:06 -0500

    implemented split_nodes_delimiter function

---
+++ src/split_nodes_delimiter.py
@@ -0,0 +1,29 @@
+from textnode import *
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    
+    return_list = []
+    
+    for node in old_nodes:
+        if node.text_type != "TEXT":
+            return_list.append(node)
+        
+        else:    
+            split_list = node.text.split(delimiter)
+            if len(split_list) % 2 == 0:
+                raise Exception("unclosed delimiter error")
+
+            if len(split_list) == 1:
+                # delimiter is not found; split did not occur
+                return_list.append(TextNode(node.text, TextType.TEXT))
+
+            else:
+                # split occurred and delimiters are valid pairs
+                for index, part in enumerate(split_list):
+                    if index % 2 == 0: # even index
+                        return_list.append(TextNode(part, TextType.TEXT))
+
+                    else: # odd index
+                        return_list.append(TextNode(part, text_type))
+
+    return return_list
